c2075fccc6f957c18667c8458d59d99923b67619
naveen99996 <naveennunna789@gmail.com>
2023-06-28 12:59:14 +0000

    solution

---
+++ src/components/Message/index.js
@@ -0,0 +1,10 @@
+// Write your code here
+import './index.css'
+
+const Message = props => {
+  const {content} = props
+
+  return <h1 className="page-content">{content}</h1>
+}
+
+export default Message
